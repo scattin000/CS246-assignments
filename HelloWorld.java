d2f40df89bb5f60c8bfe5e47e58843c81e03de3d
Sarah <Sarah@Sarahs-MacBook-Pro.local>
2016-10-03 09:50:04 -0600

    changed the text

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ import static java.lang.System.out;
 
 class World {
   public static void main(String args[]) {
-        System.out.println("Hello World!");
+        System.out.println("Hello GitHub World!");
     }
 }
