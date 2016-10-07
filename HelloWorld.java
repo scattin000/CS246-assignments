740f8fc312cc44e6cce637ab76f7fd3e004a2d50
sarah <scattin000@gmail.com>
2016-10-06 18:07:12 -0600

    added the second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ import static java.lang.System.out;
 class World {
   public static void main(String args[]) {
         System.out.println("Hello GitHub World!");
+	System.out.println("Testing...");
     }
 }
